33b7b5ba7dce9496d7c02688a27b86bbaf6b5de4
Ian MacDonald <ianmacdonald@Ian-MacDonalds-MacBook-Pro.local>
2019-01-12 20:55:48 -0330

    a bit more work

---
+++ data.py
@@ -0,0 +1,33 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'name': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error doloremque asperiores veniam itaque corrupti id iste eveniet nihil, totam placeat exercitationem, ea, deserunt tempore excepturi labore porro ad nam aliquid.',
+            'author': 'Ian MacDonald',
+            'created_date': '29-05-2018'
+        },
+        {
+            'id':2,
+            'name': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error doloremque asperiores veniam itaque corrupti id iste eveniet nihil, totam placeat exercitationem, ea, deserunt tempore excepturi labore porro ad nam aliquid.',
+            'author': 'Captain Crunch',
+            'created_date': '29-05-2018'
+        },
+        {
+            'id':3,
+            'name': 'Article three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error doloremque asperiores veniam itaque corrupti id iste eveniet nihil, totam placeat exercitationem, ea, deserunt tempore excepturi labore porro ad nam aliquid.',
+            'author': 'Mr Business',
+            'created_date': '29-05-2018'
+        },
+        {
+            'id':4,
+            'name': 'Article four',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error doloremque asperiores veniam itaque corrupti id iste eveniet nihil, totam placeat exercitationem, ea, deserunt tempore excepturi labore porro ad nam aliquid.',
+            'author': 'Map Man',
+            'created_date': '29-05-2018'
+        }
+    ]
+
+    return articles
